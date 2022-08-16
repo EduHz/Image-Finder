6db86e5e484752c73a86e6e14478437de9e3e3d6
EduHz <106116546+EduHz@users.noreply.github.com>
2022-08-16 14:10:18 -0300

    Update Tuesday

--- src/components/Paginacion.js
+++ src/components/Paginacion.js
@@ -4,8 +4,8 @@ const Paginacion = props => {
     return (
         <div className='py-3'>
             <button onClick={props.paginaAnterior} type="butoon" className='btn btn-info mr-1'>Anterior &larr;
-             </button>
-            <button onClick={props.paginaSiguiente} type="butoon" className='btn btn-info'>Siguiente &rarr; 
+            </button>
+            <button onClick={props.paginaSiguiente} type="butoon" className='btn btn-info'>Siguiente &rarr;
             </button>
         </div>
     )
